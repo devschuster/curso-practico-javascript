0fbedb5fcb881c888ab19655941f7b4cece870a6
devschuster <andres.schuster@gmail.com>
2021-09-13 14:19:31 -0500

    Teminado el tutorial del primer taller. Como tarea debemos crear el formulario y funciones para los triángulos y círculos.

--- figurasGeometricas/js/script.js
+++ figurasGeometricas/js/script.js
@@ -1,1 +1,64 @@
-console.log("Hello World");
+// Código del Cuadrado
+console.group("Cuadrados");
+function perimetroCuadrado(lado) {
+    return lado * 4;
+} 
+
+function areaCuadrado(lado) {
+    return lado * lado;
+} 
+
+console.groupEnd();
+
+// Código del Tríangulo
+console.group("Triangulos");
+
+function perimetroTriangulo(ladoA, ladoB, base) {
+    return ladoA + ladoB + base;
+}
+
+function areaTriangulo (base, altura){
+    return (base * altura) / 2;
+}
+
+
+console.groupEnd();
+
+// Código del Círculos
+console.group("Círculos");
+
+// Diametro
+function diametroCirculo (radio) {
+    return radio * 2;
+}
+
+// PI
+const PI = Math.PI;
+
+// Circunferencia
+function perimetroCirculo (radio){
+    const diametro = diametroCirculo (radio);
+    return diametro * PI;
+} 
+
+// Área
+function areaCirculo (radio) {
+    return (radio * radio) * PI
+}
+
+console.groupEnd();
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("inputLadoCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+function calcularAreaCuadrado() {
+    const input = document.getElementById("inputLadoCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
